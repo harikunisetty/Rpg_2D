db90457921e5fe6f078f87873135f795dea59eb1
harikunisetty <86394723+harikunisetty@users.noreply.github.com>
2021-11-01 16:49:57 +0530

    update

--- Assets/Scripts/Key/KeyObject.cs
+++ Assets/Scripts/Key/KeyObject.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class KeyObject : MonoBehaviour
 {
-    public GameObject levelObject;
+  public SpringJoint2D spring;
 
     public void Start()
     {
-       
+        spring = GetComponent<SpringJoint2D>();
+        spring.enabled = false;
+        GameObject backpack = GameObject.FindWithTag("BackPack");
+        spring.connectedBody = backpack.GetComponent<Rigidbody2D>();
     }
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            spring.enabled = true;
+        }
+           
+    }
 }
--- Assets/Scripts/LevelChanger/LevelChangerObject.cs
+++ Assets/Scripts/LevelChanger/LevelChangerObject.cs
@@ -15,8 +15,6 @@ public class LevelChangerObject : MonoBehaviour
     {
         isObjectiveCompleted = false;
     }
-
-    // Update is called once per frame
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
